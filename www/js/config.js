3decc9ab72570870edb0c554de86207c919f5213
carterax <ikenna750@gmail.com>
2018-02-07 07:47:51 +0100

    res changes

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={		
+	'ApiUrl':"http://home.skyearning.com/driver/api",	
+	'DialogDefaultTitle':"Zoulak Driver App",	
+	'PushProjectID':"600405925358",	
+	'APIHasKey':"unique"
+};
